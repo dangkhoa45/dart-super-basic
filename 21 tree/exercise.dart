73aecd8b8d05182636f43c57c8f3c3ab6a9db9b2
Khoa Đặng <kd03102014@gmail.com>
2022-07-20 14:00:21 +0700

    the end

--- 21 tree/exercise.dart
+++ 21 tree/exercise.dart
@@ -11,25 +11,76 @@
 // function that gets called with each element in the tree
 
 class Node {
-  
+  dynamic data;
+  List<Node> children = [];
+  Node(this.data);
+  void add(dynamic child) {
+    children.add(Node(child));
+  }
+
+  void remove(dynamic child) {
+    for (var i = 0; i < children.length; i++) {
+      if (children[i].data == child) {
+        children.removeAt(i);
+      }
+    }
+  }
 }
 
 class Tree {
-  Node root;
+  Node? root;
 
   _traverseBF(Node baseNode, Function(Node) fn) {
-    
+    baseNode = root ?? Node(-99);
+    List<Node> listNode = [];
+    listNode.add(baseNode);
+    var index = 0;
+    while (true) {
+      Node currentNode = listNode[index];
+      int numberOfChild = currentNode.children.length;
+      for (var i = 0; i < numberOfChild; i++) {
+        listNode.add(currentNode.children[i]);
+      }
+      index++;
+      if (index > listNode.length - 1) break;
+    }
+    for (Node node in listNode) fn(node);
   }
 
   _traverseDF(Node baseNode, Function(Node) fn) {
-    
+    baseNode = root ?? Node(-99);
+    List<Node> listNodeTmp = [];
+    List<Node> listNodeStack = [];
+    List<Node> storeNodeTraversed = [];
+    listNodeTmp.add(baseNode);
+
+    while (true) {
+      Node currentNode = listNodeTmp.elementAt(listNodeTmp.length - 1);
+      int numberOfChild = currentNode.children.length;
+
+      if (storeNodeTraversed.contains(currentNode)) {
+        listNodeStack.add(currentNode);
+        listNodeTmp.removeLast();
+        if (listNodeTmp.length == 0)
+          break;
+        else
+          continue;
+      } else {
+        for (var i = 0; i < numberOfChild; i++) {
+          listNodeTmp.add(currentNode.children[i]);
+        }
+        storeNodeTraversed.add(currentNode);
+      }
+    }
+    var resultList = listNodeStack.reversed;
+    for (Node node in resultList) fn(node);
   }
 
   traverseBreadthFirst(Function(Node) fn) {
-    
+    _traverseBF(root ?? Node(-99), fn);
   }
 
   traverseDepthFirst(Function(Node) fn) {
-    
+    _traverseDF(root ?? Node(-99), fn);
   }
 }
--- 26 sorting/exercise.dart
+++ 26 sorting/exercise.dart
@@ -2,17 +2,72 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 List<int> bubbleSort(List<int> arr) {
-  return sorted;
+  for (var i = 1; i < arr.length; i++) {
+    for (var j = 0; j < i; j++) {
+      if (arr[j] > arr[i]) {
+        //Swap
+        var temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+      }
+    }
+  }
+  return arr;
 }
 
 List<int> selectionSort(List<int> arr) {
+  for (var i = 0; i < arr.length; i++) {
+    int min = arr[i];
+    int index = i;
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[j] < min) {
+        min = arr[j];
+        index = j;
+      }
+    }
+    //Swap
+    var temp = arr[i];
+    arr[i] = arr[index];
+    arr[index] = temp;
+  }
   return arr;
 }
 
 List<int> mergeSort(List<int> arr) {
- 
+  var left = 0;
+  var right = arr.length - 1;
+  var mid = (left + right) ~/ 2;
+  if (arr.length == 1) return arr;
+  var leftArr = arr.getRange(left, mid + 1);
+  var rightArr = arr.getRange(mid + 1, right + 1);
+  return merge(mergeSort(leftArr.toList()), mergeSort(rightArr.toList()));
 }
 
 List<int> merge(List<int> left, List<int> right) {
-  
+  var lengthLeft = left.length;
+  var lengthRight = right.length;
+  var indexLeft = 0;
+  var indexRight = 0;
+
+  List<int> mix = [];
+  while ((indexLeft < lengthLeft) && (indexRight < lengthRight)) {
+    if (left[indexLeft] < right[indexRight]) {
+      mix.add(left[indexLeft]);
+      ++indexLeft;
+    } else {
+      mix.add(right[indexRight]);
+      ++indexRight;
+    }
+  }
+
+  while (indexLeft < lengthLeft) {
+    mix.add(left[indexLeft]);
+    indexLeft++;
+  }
+
+  while (indexRight < lengthRight) {
+    mix.add(right[indexRight]);
+    indexRight++;
+  }
+  return mix;
 }
